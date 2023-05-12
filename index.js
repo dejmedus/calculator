95bb0235d96c3bc4bf199bcfb33d9f8d6c891275
mete <25986959+mtoprak53@users.noreply.github.com>
2023-05-12 17:54:09 -0400

    css done

--- index.js
+++ index.js
@@ -6,55 +6,66 @@ let currentOperator = '';
 let afterOp = false;
 
 buttons.addEventListener('click', (e) => {
-    const buttonId = e.target.id;
+    const buttonValue = e.target.id;
 
-    if (Number.isInteger(+buttonId)) {
+    if (Number.isInteger(+buttonValue)) {
         if (afterOp) {
             screen.innerText = '0';
             afterOp = false;
         }
         if (screen.innerText.length < 8) {
-            if (screen.innerText == '0') {
-                screen.innerText = buttonId;
-            }
-            else {
-                screen.innerText = +screen.innerText * 10 + (+buttonId);
-            }
+            screen.innerText = +screen.innerText * 10 + (+buttonValue);
         }
-
     }
-    else if (buttonId !== 'equals' && buttonId !== 'C') {
+    else if (buttonValue !== 'equals' && buttonValue !== 'C') {
         if (currentOperator) {
-            console.log(currentTotal, currentOperator, screen.innerText);
+            // console.log(currentTotal, currentOperator, screen.innerText);
             currentTotal = operations[currentOperator](currentTotal, +screen.innerText);
-            screen.innerText = currentTotal;
+            screen.innerText = trimmer(currentTotal);
         }
         // is operation button but not equals
         afterOp = true
         currentTotal = +screen.innerText;
-        currentOperator = buttonId;
+        currentOperator = buttonValue;
     }
-    else if (buttonId === 'equals') {
+    else if (buttonValue === 'equals' && currentOperator) {
         // is equals
-        console.log(currentTotal, currentOperator, screen.innerText);
+        // console.log(currentTotal, currentOperator, screen.innerText);
         currentTotal = operations[currentOperator](currentTotal, +screen.innerText);
-        screen.innerText = currentTotal;
+        screen.innerText = trimmer(currentTotal);
     }
     else {
         // is clear
         currentTotal = 0;
         currentOperator = '';
         screen.innerText = '0';
-        console.log(currentTotal, currentOperator, screen.innerText);
+        // console.log(currentTotal, currentOperator, screen.innerText);
     }
-
 })
 
 const operations = {
     'add': (num1, num2) => num1 + num2,
     'subtract': (num1, num2) => num1 - num2,
     'divide': (num1, num2) => num1 / num2,
-    'multiply': (num1, num2) => num1 * num2
+    'multiply': (num1, num2) => num1 * num2,
 }
 
+function trimmer(currentTotal){
+    const leftSide = Math.floor(currentTotal);
+    const rightSide = currentTotal - leftSide;
+    const leftDigits = leftSide.toString().length;
 
+    console.log(leftSide, rightSide, leftDigits);
+    if (leftDigits > 8) {
+
+        currentTotal = 0;
+        currentOperator = '';
+        afterOp = false;
+        return "ERR";
+    }            
+    else {
+        const rightDigitsPower = 10 ** (8 - leftDigits);
+        const trimmedRightSide = Math.floor(rightDigitsPower * rightSide) / rightDigitsPower;
+        return leftSide + trimmedRightSide;
+    }
+}
